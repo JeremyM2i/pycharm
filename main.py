4d6e4c5826db2f743fdc68be9d312ec5c710794b
JeremyM2i <jeremy.dehors@labom2iformation.fr>
2021-01-12 14:58:25 +0100

    on ajoute les classes

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-casino = Casino(100000)
-jeu1 = Roulette("my wheel", casino)
-jeu2 = MachineASous("bandit manchot", casino)
-joueur = Joueur("Clement", 1000)
-joueur.entrer_casino(casino)
-joueur.jouer()
+from casino import Casino
+from jeu import Jeu
+from roulette import Roulette
+from joueur import Joueur
+from machineasous import MachineASous
+
+
+def main():
+    casino = Casino(100000)
+    jeu1 = Roulette("my wheel", casino)
+    jeu2 = MachineASous("bandit manchot", casino)
+    joueur = Joueur("Clement", 1000)
+    joueur.entrer_casino(casino)
+    joueur.jouer()
 
